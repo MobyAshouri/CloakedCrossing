0548447066f0715924ddc8d66fc9e7c1ef1eb0ee
MobyAshouri <droopychicken1@gmail.com>
2023-05-05 22:25:08 -0500

    Added a Main Menu, assets, and font

--- CloakedCrossing.py
+++ CloakedCrossing.py
@@ -4,20 +4,49 @@ import math
 pg.init()
 pg.display.set_caption("Cloaked Crossing")
 pg.display.set_icon(pg.image.load("images/icon.jpg"))
-screen = pg.display.set_mode((1280, 720))
-clock = pg.time.Clock()
-running = True
-dt = 0
 
+screenWidth = 1280
+screenHeight = 720
+
+screen = pg.display.set_mode((screenWidth, screenHeight))
+clock = pg.time.Clock()
+dt = 0
 
 
 class Level:
 
     def __init__(self) -> None:
         self.floorY = 600
+        self.pastMenu = True
+        self.running = True
+        
 
     def drawFloor(self):
         pg.draw.rect(screen, "black", pg.Rect(0, self.floorY, 1280, 120))
+
+    def drawMenu(self):
+        startButtonWidth = 450
+        startButtonHeight = 180
+        startButton = pg.image.load("images/menu/assets/startButton.png")
+        startButton = pg.transform.scale(startButton, (startButtonWidth, startButtonHeight))
+        screen.blit(startButton, (((screenWidth/2)-(startButtonWidth/2)), 200))
+
+        if event.type == pg.MOUSEBUTTONDOWN:
+            mouse = pg.mouse.get_pos()
+            if (468 <= mouse[0]<=816 and 218 <= mouse[1] <= 365):
+                self.pastMenu = True
+                self.running = False
+                level.fadeOut()
+
+    def fadeOut(self):
+        fade = pg.Surface((screenWidth, screenHeight))
+        fade.fill((0,0,0))
+        fade.set_alpha(0)
+        for alpha in range(0,255):
+            fade.set_alpha(alpha)          #kind of works
+            screen.blit(fade, (0,0))
+            pg.display.update()
+            pg.time.delay(5)
 
 
 
@@ -58,8 +87,6 @@ class Player:
             "images/runRight/runRight5.png",
             "images/runRight/runRight6.png",
         ]
-
-        
 
     def movePlayer(self, x, y):
         self.posX = x
@@ -103,23 +130,29 @@ class Player:
         pass
 
 
-
-
 player = Player("images/stickFigure.png")
+level = Level()
 
-while running:
+while level.running:
+    for event in pg.event.get():
+        if event.type ==pg.QUIT:
+            level.running = False
+            level.pastMenu = False
+    screen.fill("white")
+    level.drawMenu()
+
+    pg.display.flip()
+    dt = clock.tick(144) / 1000
+
+
+while level.pastMenu:
     for event in pg.event.get():
         if event.type == pg.QUIT:
-            running = False
+            level.pastMenu = False
 
-    # fill the screen with a color to wipe away anything from last frame
     screen.fill("white")
-
-    player.spriteCounter+=1
-
-    level = Level()
     level.drawFloor()
-    
+
     keys = pg.key.get_pressed()
 
     if keys[pg.K_SPACE] or keys[pg.K_w]:
@@ -129,7 +162,6 @@ while running:
 
     if keys[pg.K_s]:
         player.crouch()
-        pass
     if keys[pg.K_a]:
         player.movePlayer(player.posX-player.movementSpeed, player.posY)
     if keys[pg.K_d]:
